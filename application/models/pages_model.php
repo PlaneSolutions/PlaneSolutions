56ba725d9c1e73c7665398be06d25fd25bf55709
Plane Solutions <asad.star.leo@gmail.com>
2015-04-06 20:10:31 +0500

    XML to Jason and updates some more values in campaing and iamges table

---
+++ application/models/pages_model.php
@@ -0,0 +1,25 @@
+<?php
+class Pages_model extends CI_Model {
+
+    function get_all_images($user_id,$campaing_id){
+        $query = $this->db->get_where('images',array('u_id' => $user_id,'campaing_id' => $campaing_id));
+        return $query->result();
+    }
+    function uploadimage($image_data = array(), $other_data){
+        /*print_r($image_data);die;*/
+        $data = array(
+            'name' => $image_data['upload_data']['file_name'],
+            'width' => $image_data['upload_data']['image_width'],
+            'height' => $image_data['upload_data']['image_height'],
+            'type' => $other_data['type'],
+            'url' => $image_data['upload_data']['full_path'],
+            'u_id' => $other_data['u_id'],
+            'campaing_id' => $other_data['campaign_id'],
+        );
+        $this->db->insert('images',$data);
+        $this->db->where('id', $other_data['campaign_id']);
+        $this->db->set('number_of_pages', 'number_of_pages+1', FALSE);
+        $this->db->update('campaings');
+    }
+}
+?>
